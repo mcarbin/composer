5374c23cdb668c53fbc7adef774d49f65a0d7ca5
Michael Carbin <michael.carbin@databricks.com>
2023-09-17 09:23:39 -0400

    update

--- composer/datasets/in_context_learning_evaluation.py
+++ composer/datasets/in_context_learning_evaluation.py
@@ -1125,7 +1125,7 @@ class GroupedSampler(DistributedSampler):
         self.excess = len(self.dataset) % block_size
         self.padding_size = (block_size - self.excess) if self.excess != 0 else 0
         self.total_size = (len(self.dataset) + self.padding_size)
-        self.num_samples = self.total_size / self.num_replicas
+        self.num_samples = self.total_size // self.num_replicas
      
         print(f'datset_length: {len(self.dataset)} block_size: {block_size} excess: {self.excess} padding_size: {self.padding_size} total_size: {self.total_size} num_samples: {self.num_samples}')  
         assert (self.total_size % self.num_replicas) == 0
